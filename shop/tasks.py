712d3f69dd73d63f3550391fca34bc918e91915b
Nemtsov <mrtwister5950@gmail.com>
2020-04-12 23:30:41 +0300

    cahnged taskt

--- shop/tasks.py
+++ shop/tasks.py
@@ -1,13 +1,30 @@
 import time
 from datetime import timedelta
-from django.core.mail import send_mail
 
+from celery.schedules import crontab
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from shop.models import Product
+from amaizing_shop import settings
 
 @shared_task
 def hard_logic():
     time.sleep(5)
     return "It works!"
 
+def mailing_list():
+    user_emails = dict(User.objects.exclude(email='')).values_list('username', 'email')
+    return user_emails
+
+
+def new_products():
+    products = Product.objects.filter(
+        created_at__gte=timezone.now() - timedelta(
+            days=settings.NEW_PRODUCT_DAYS)
+    )
+    list_products = [f'{product.title}: {product.price}$' for product in
+                     products]
+    return '\n'.join(list_products)
 
 @shared_task
 def my_periodic_logic():
@@ -20,12 +37,19 @@ SCHEDULE = {
         'args': (),
         'options': {},
         'schedule': timedelta(seconds=5)
+    },
+    'send_email_task': {
+        'task': 'shop.tasks.send_email_task',
+        'args': (),
+        'options': {},
+        'schedule': crontab(minute='30', hour='9', day_of_week='mon')
     }
 }
 
 @shared_task
 def send_email_task():
-    products = Products.obj.all
+    products = new_products()
+    users = mailing_list()
     send_mail(
         'New products',
         f'HelloThe list of new products:',
